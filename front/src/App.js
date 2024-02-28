e38fc3870e0916d920f1f882bab80c6d9ab0a9a6
jwlee <tlstprhl12@hansung.ac.kr>
2024-02-28 11:34:11 +0900

    Src에 jsxfiles,cssfiles 추가 App.js에 router,경로 추가

--- front/src/App.js
+++ front/src/App.js
@@ -1,19 +1,39 @@
-import {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import StartPage from './jsxfiles/startPage';
+import LoginPage from './jsxfiles/loginPage';
+import SignupPage from './jsxfiles/signupPage';
 import axios from "axios";
 
 function App() {
     const [hello, setHello] = useState('');
+    const [showText, setShowText] = useState(false);
 
     useEffect(() => {
-    axios.get('/api/test')
-        .then((res) => {
-            setHello(res.data);
-        })
+        setTimeout(() => {
+            setShowText(true);
+        }, 1000);
+
+        setTimeout(() => {
+            setShowText(false);
+        }, 4000);
     }, []);
+/*
+    useEffect(() => {
+        axios.get('/api/test')
+            .then((res) => {
+                setHello(res.data);
+            })
+    }, []);
+*/
     return (
-        <div className="App">
-        백엔드 데이터 : {hello}
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<StartPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignupPage />} />
+            </Routes>
+        </Router>
     );
 }
 
