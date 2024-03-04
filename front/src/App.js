8a71477c28422ba424489973bc99fa5544baf69b
hs-1971166-yunhonam <nam100400@naver.com>
2024-03-04 13:32:24 +0900

    백 - 로그인 구현
    - 로그인 구현 완료
    - 로그아웃은 아직 미구현

--- front/src/App.js
+++ front/src/App.js
@@ -1,33 +1,66 @@
-import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import StartPage from './jsxfiles/startPage';
-import MainPage from './jsxfiles/mainPage';
-import ChatPage from './jsxfiles/chatPage';
-import RloginPage from './jsxfiles/rloginPage';
+// import React, { useEffect, useState } from "react";
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import StartPage from './jsxfiles/startPage';
+// import LoginPage from './jsxfiles/loginPage';
+// import SignupPage from './jsxfiles/signupPage';
+// import axios from "axios";
+//
+// function App() {
+//     const [hello, setHello] = useState('');
+//     const [showText, setShowText] = useState(false);
+//
+//     useEffect(() => {
+//         setTimeout(() => {
+//             setShowText(true);
+//         }, 1000);
+//
+//         setTimeout(() => {
+//             setShowText(false);
+//         }, 4000);
+//     }, []);
+//     return (
+//         <Router>
+//             <Routes>
+//                 <Route path="/" element={<StartPage />} />
+//                 <Route path="/login" element={<LoginPage />} />
+//                 <Route path="/signup" element={<SignupPage />} />
+//             </Routes>
+//         </Router>
+//     );
+// }
+//
+// export default App;
 
-import axios from "axios";
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import MyPage from './pages/MyPage';
+import { useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import { getUserInfo } from './api/getUserInfo';
 
-function App() {
-    const [hello, setHello] = useState('');
+export default function App() {
+    const [isLogin, setIsLogin] = useState(false);
 
-/*
     useEffect(() => {
-        axios.get('/api/test')
-            .then((res) => {
-                setHello(res.data);
-            })
+        const initLogin = async () => {
+            const name = await getUserInfo();
+            setIsLogin(!!name);
+        };
+        initLogin();
     }, []);
-*/
+
     return (
-        <Router>
+        <div className="App">
             <Routes>
-                <Route path="/" element={<StartPage />} />
-                <Route path="/rlogin" element={<RloginPage />} />
-                <Route path="/main" element={<MainPage />} />
-                <Route path="/chat" element={<ChatPage />} />
+                <Route
+                    path="/"
+                    element={<Login isLogin={isLogin} setIsLogin={setIsLogin} />}
+                />
+                <Route
+                    path="mypage"
+                    element={isLogin ? <MyPage isLogin={isLogin} /> : <Navigate to="/" />}
+                />
             </Routes>
-        </Router>
+        </div>
     );
 }
-
-export default App;
---
+++ front/src/pages/MyPage.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import { useState } from 'react';
+import { getUserInfo } from '../api/getUserInfo';
+
+export default function MyPage({ isLogin }) {
+    const navigate = useNavigate();
+    const [info, setInfo] = useState({
+        email: '',
+        firstName: '',
+        lastName: '',
+    });
+
+    useEffect(() => {
+        if (!isLogin) navigate('/');
+
+        const initUserinfo = async () => {
+            const newinfo = await getUserInfo();
+            setInfo(newinfo);
+        };
+        initUserinfo();
+    }, [isLogin]);
+
+    return (
+        <div>
+            <h1>mypage</h1>
+            <p>Welcome To MyPage</p>
+            <p>email: {info.email}</p>
+            <p>name: {`${info.lastName} ${info.firstName}`}</p>
+            <p><Link to="/">goto Login Page</Link></p>
+        </div>
+
+    );
+}
