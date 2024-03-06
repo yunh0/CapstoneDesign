00551d1c0b4ad39edd6a846a17e1060b35bc1e62
jwlee <tlstprhl12@hansung.ac.kr>
2024-03-06 17:13:42 +0900

    챗페이지 옆의 사이드바 구성 - 버튼 맨위로, 모달창 어떻게 할지,summit해서 보내기 등 미정

--- front/src/index.js
+++ front/src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Ap from './jsxfiles/chatPage';
 
 createRoot(document.getElementById('root')).render(
     <React.StrictMode>
