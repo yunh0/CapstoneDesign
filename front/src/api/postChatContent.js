804535edb33b88c3e4818540ccf5e7cffd00cbee
jwlee <tlstprhl12@hansung.ac.kr>
2024-03-13 17:05:15 +0900

    채팅방 css통합 3/13

--- front/src/api/postChatContent.js
+++ front/src/api/postChatContent.js
@@ -1,6 +1,6 @@
 export const postChatContent = async (content) => {
     const API_URL = process.env.REACT_APP_API_URL;
-    const path = '/api/chat';
+    const path = '/api/user/message';
 
     try {
         const response = await fetch(`${API_URL}${path}`, {
