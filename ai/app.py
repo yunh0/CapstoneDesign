3731b1f3da1ac37e3fa08136064b889ff98c95c4
jiiwons <sjiwon0402@naver.com>
2024-03-18 18:12:32 +0900

    챗봇 로직 구현
    소스id 전역변수로 선언하여 답변 생성

--- ai/app.py
+++ ai/app.py
@@ -4,24 +4,37 @@ import requests
 app = Flask(__name__)
 
 API_KEY = "sec_U8gkLvCfx4KOIyX3RwhdafCi2VoVIgJa"
+source_id = None
 
 @app.route('/api/receive', methods=['POST'])
-def receive_data():
+def receive_file_path():
     try:
         data = request.get_json()
         file_path = data.get('filePath')
-        question = data.get('question')
-
+        global source_id
         # PDF 파일에 대한 소스 ID를 가져옵니다.
         source_id = get_pdf_source_id(file_path)
 
         if source_id is None:
             return jsonify({"error": "소스 ID를 가져오는 데 실패했습니다. 파일 경로를 확인하세요."}), 500
 
-        # PDF API를 통해 질의응답을 처리합니다.
-        chatpdf_result = chat_with_pdf_bot(source_id, question)
+        return jsonify({"sourceId": source_id}), 200
 
-        return jsonify({"result": chatpdf_result}), 200
+    except Exception as e:
+        # 오류가 발생하면 오류 응답을 반환합니다.
+        error_data = {"status": "error", "message": str(e)}
+        return jsonify(error_data), 500
+
+@app.route('/api/message/receive', methods=['POST'])
+def receive_question():
+    try:
+        data = request.get_json()
+        question = data.get('question')
+
+        # chatpdf API를 통해 질문과 소스 ID에 대한 응답을 처리합니다.
+        chatpdf_response = chat_with_pdf_bot(question, source_id)
+
+        return jsonify({"response": chatpdf_response}), 200
 
     except Exception as e:
         # 오류가 발생하면 오류 응답을 반환합니다.
@@ -41,7 +54,7 @@ def get_pdf_source_id(pdf_file_path):
     else:
         return None
 
-def chat_with_pdf_bot(source_id, question):
+def chat_with_pdf_bot(question, source_id):
     endpoint = 'https://api.chatpdf.com/v1/chats/message'
     headers = {
         'x-api-key': API_KEY,
@@ -53,14 +66,14 @@ def chat_with_pdf_bot(source_id, question):
         {'role': 'assistant', 'content': ''}
     ]
 
-    response = requests.post(endpoint, headers=headers, json={'sourceId': source_id, 'messages': messages})
+    data = {'sourceId': source_id, 'messages': messages}
+
+    response = requests.post(endpoint, headers=headers, json=data)
 
     if response.status_code == 200:
         return response.json().get('content')
     else:
         return "ChatPDF API로부터 응답을 받는 데 실패했습니다."
-
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
