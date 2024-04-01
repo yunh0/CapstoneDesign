599eb5a818ff9863cfb5b0dec5635c1e194d7cca
twoallzero <115561503+twoallzero@users.noreply.github.com>
2024-04-01 19:49:15 +0900

    pdf 보여주기 완성

--- front/src/jsxfiles/pdfViewer.jsx
+++ front/src/jsxfiles/pdfViewer.jsx
@@ -2,9 +2,10 @@
 import React from 'react';
 
 const PdfViewer = ({ pdfUrl }) => {
+    console.log("In pdfViewer: ",pdfUrl);
     return (
         <div className="pdf-viewer-container" style={{height: '96%'}}>
-            <iframe src={pdfUrl} title="PDF Viewer" style={{width: '100%', height: '100%'}}/>
+            <iframe src={pdfUrl} title="PDF Viewer" style={{width: '100%', height: '100%'}} key={pdfUrl}/>
         </div>
     );
 };
