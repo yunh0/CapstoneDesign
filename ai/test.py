224f36416beef5ce91ea29f75222c9656bedfca9
jiiwons <sjiwon0402@naver.com>
2024-04-29 14:10:53 +0900

    RAG 백엔드 연동

--- ai/test.py
+++ ai/test.py
@@ -1,103 +1,159 @@
+from flask import Flask, request, jsonify
 import os
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.chat_models import ChatOpenAI
+from langchain_community.vectorstores import Chroma
+from langchain_openai import ChatOpenAI
 from langchain.agents.agent_toolkits import create_retriever_tool
 from langchain.agents.openai_functions_agent.base import OpenAIFunctionsAgent
 from langchain.schema.messages import SystemMessage
 from langchain.prompts import MessagesPlaceholder
 from langchain.agents import AgentExecutor
 
-# 사용자가 보낸 질문에 마우스 호버 하면(또는 각 질문 위에)
-# <타 보험과 비교하기> 버튼을 눌러서 그 질문이 이 파일에 넘어와서 질문을 하도록 하기
-os.environ["OPENAI_API_KEY"] = "YOUR_API_KEY"
-# llm 셋업
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+# Flask 애플리케이션 생성
+app = Flask(__name__)
 
-loaders = [
-    PyPDFLoader("./file/(무)퇴직보험_약관_20130612.pdf"),
-]
+# 파일 경로 저장을 위한 전역 변수
+file_path = None
 
-docs = []
-for loader in loaders:
-    docs.extend(loader.load())
 
-# 문서를 청크로 분할
-# chunk_size는 자르고자 하는 글자수, chunk_overlap 은 각 부분마다 겹쳐도 되는 글자 수
-# Recursive Character Text Splitter 는 최대한 관련 있는 문장들을 같이 묶어서 잘라주는 기능을 한다.
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=0,
-)
-# docs 변수에 분할 문서를 저장
-splits = text_splitter.split_documents(docs)
+# 파일 경로를 받아서 벡터스토어에 저장하는 함수
+@app.route('/api/receive', methods=['POST'])
+def receive_file():
+    global file_path
+    # 파일 경로 받기
+    file_path = request.json.get('filePath')
+    print(file_path)
+    # 응답 반환
+    return jsonify({'message': 'File received successfully.'})
 
-# OpenAI 임베딩 모델을 사용하여 텍스트를 벡터로 변환하고, 이를 Chroma 벡터 저장소에 저장
-embedding = OpenAIEmbeddings()
-vectorstore = Chroma.from_documents(documents=splits, embedding=embedding)
 
-vectorstore.persist()
-print(vectorstore._collection.count(), "documents in collection saved.")
+# 사용자가 질문을 보내면 이 함수가 호출됨
+@app.route('/api/message/receive', methods=['POST'])
+def receive_message():
+    try:
+        global file_path
+        # JSON 형식으로 데이터 받기
+        data = request.get_json()
 
-# LLM 에이전트가 사용할 수 있도록 retriever 라는 툴로 만들기
-retriever = vectorstore.as_retriever()
+        # 질문 받기
+        question = data.get('content')
 
-# LLM에이전트가 만약 유저가 내가 업로드한 문서에 관련된 질문을 하면 이 tool을 사용해서 검색을 하게 됨
-tool = create_retriever_tool(
-    retriever,
-    "cusomter_service",
-    "Searches and returns documents regarding the customer service guide.",
-)
-tools = [tool]
+        # 파일 경로가 없으면 오류 메시지 반환
+        if file_path is None:
+            return jsonify({'response': '파일이 업로드되지 않았습니다.'})
 
-# 대화 내용 기억할 수 있는 메모리 부여
-memory_key = "history"
+        # 질문에 대한 답변 생성
+        response = answer_question(question, file_path)
+        response_data = {"status": "success", "message": response}
+        # 답변 반환
+        return jsonify(response_data), 200
 
-from langchain.agents.openai_functions_agent.agent_token_buffer_memory import (
-    AgentTokenBufferMemory,
-)
+    except Exception as e:
+        # 오류가 발생하면 오류 응답을 반환합니다.
+        error_data = {"status": "error", "message": str(e)}
+        return jsonify(error_data), 500
 
-memory = AgentTokenBufferMemory(memory_key=memory_key, llm=llm)
 
-# 에이전트 만들기(할루시네이션 전처리)
-# LLM이 어떤 행동을 해야하는지 알려주는 프롬프트
-system_message = SystemMessage(
-    content=(
-        # 여기서는 챗봇에게 너는 어떤 챗봇인지 설명해주고, 위에서 만든 툴을 활용해서 답변을 하라고 해줌
-        "You are a nice customer Insurance service agent."
-        "Do your best to answer the questions."
-        "Feel free to use any tools available to look up "
-        "relevant information, only if necessary"
-        # 할루시네이션 전처리 과정, 프롬프트로 "만약 보험약관과 관련되지 않은 질문이면 거짓된 답변을 생성하지 말아줘" 라고 처리
-        "Please do not generate incorrect answers to questions that are not related to insurance terms and conditions"
-        "If you don't know the answer, just say that you don't know. Don't try to make up an answer."
-        # 챗봇이 한국어로 대답하게 함
-        "Make sure to answer in Korean."
+def answer_question(question, file_path):
+    if file_path is None:
+        return "파일 경로가 잘못되었습니다."
+
+    # 벡터 스토어에 저장하고 반환
+    vectorstore = store_vectors(file_path)
+    if vectorstore is None:
+        return "파일을 로드하는 동안 오류가 발생했습니다."
+
+    # LLM 에이전트가 사용할 수 있도록 retriever 라는 툴로 만들기
+    retriever = vectorstore.as_retriever()
+
+    # LLM에이전트가 만약 유저가 내가 업로드한 문서에 관련된 질문을 하면 이 tool을 사용해서 검색을 하게 됨
+    tool = create_retriever_tool(
+        retriever,
+        "customer_service",
+        "Searches and returns documents regarding the customer service guide.",
     )
-)
+    tools = [tool]
 
-# 위에서 만든 프롬프트를 바탕으로 LLM에게 줄 프롬프트 형식 셋업
-prompt = OpenAIFunctionsAgent.create_prompt(
-    system_message=system_message,
-    extra_prompt_messages=[MessagesPlaceholder(variable_name=memory_key)],
-)
+    # 대화 내용 기억할 수 있는 메모리 부여
+    memory_key = "history"
+    from langchain.agents.openai_functions_agent.agent_token_buffer_memory import AgentTokenBufferMemory
+    memory = AgentTokenBufferMemory(memory_key=memory_key, llm=llm)
 
-# 위에서 만든 llm, tools, prompt를 바탕으로 에이전트 만들어주기
-agent = OpenAIFunctionsAgent(llm=llm, tools=tools, prompt=prompt)
+    # 에이전트 만들기
+    system_message = SystemMessage(
+        content=(
+            "You are a nice customer Insurance service agent."
+            "Do your best to answer the questions."
+            "Feel free to use any tools available to look up "
+            "relevant information, only if necessary"
+            "Please do not generate incorrect answers to questions that are not related to insurance terms and conditions"
+            "If you don't know the answer, just say that you don't know. Don't try to make up an answer."
+            "Make sure to answer in Korean."
+        )
+    )
+    prompt = OpenAIFunctionsAgent.create_prompt(
+        system_message=system_message,
+        extra_prompt_messages=[MessagesPlaceholder(variable_name=memory_key)],
+    )
+    agent = OpenAIFunctionsAgent(llm=llm, tools=tools, prompt=prompt)
 
-# 에이전트 실행하기
-# 여기를 플라스크에서 질문을 받아오도록 바꾸기
-from langchain.agents import AgentExecutor
+    # 에이전트 실행
+    agent_executor = AgentExecutor(
+        agent=agent,
+        tools=tools,
+        memory=memory,
+        verbose=True,
+        return_intermediate_steps=True,
+    )
 
-agent_executor = AgentExecutor(
-    agent=agent,
-    tools=tools,
-    memory=memory,
-    verbose=True,
-    return_intermediate_steps=True,
-)
+    # 위에서 작성한 agent_executor 사용하여 질문에 대한 답변 생성
+    result = agent_executor({"input": question})
+    response = result.get('output', None)
+    if response is None:
+        return "챗봇이 답변을 생성하지 못했습니다."
+    else:
+        return response
 
-result = agent_executor({"input": "안녕하세요"})
-print(result)
+
+# 파일 경로를 받아서 벡터스토어에 저장하는 함수
+def store_vectors(file_path):
+    if file_path is None:
+        return None
+
+    # 파일로부터 문서 로드
+    loaders = [
+        PyPDFLoader(file_path),
+    ]
+
+    docs = []
+    for loader in loaders:
+        docs.extend(loader.load())
+
+    # 문서를 청크로 분할
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=0,
+    )
+    splits = text_splitter.split_documents(docs)
+
+    # OpenAI 임베딩 모델을 사용하여 텍스트를 벡터로 변환하고, 이를 Chroma 벡터 저장소에 저장
+    embedding = OpenAIEmbeddings()
+    vectorstore = Chroma.from_documents(documents=splits, embedding=embedding)
+
+    vectorstore.persist()
+    print(vectorstore._collection.count(), "documents in collection saved.")
+
+    return vectorstore  # 생성된 vectorstore 반환
+
+
+# 애플리케이션 실행
+if __name__ == '__main__':
+    # 초기 설정
+    os.environ["OPENAI_API_KEY"] = ""
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+
+    # 애플리케이션 실행
+    app.run(debug=True)
+
