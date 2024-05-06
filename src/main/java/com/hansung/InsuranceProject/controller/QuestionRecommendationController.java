a09b8feaf6f272d6f8149fd21ac74e87e4ebf81a
hs-1971166-yunhonam <nam100400@naver.com>
2024-05-06 19:00:18 +0900

    백) 프론트와 통신할 때 문제 해결

--- src/main/java/com/hansung/InsuranceProject/controller/QuestionRecommendationController.java
+++ src/main/java/com/hansung/InsuranceProject/controller/QuestionRecommendationController.java
@@ -4,10 +4,7 @@ import com.hansung.InsuranceProject.dto.QuestionRecommendationDto;
 import com.hansung.InsuranceProject.service.QuestionRecommendationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,7 +15,7 @@ public class QuestionRecommendationController {
     @Autowired
     QuestionRecommendationService questionRecommendationService;
 
-    @GetMapping("/sreCo/{chatRoomId}")
+    @PostMapping("/sreCo/{chatRoomId}")
     public ResponseEntity userQuestionRecommendation(@PathVariable Long chatRoomId){
 
         List<String> questionRecommendation = questionRecommendationService.getRecommendations(chatRoomId);
