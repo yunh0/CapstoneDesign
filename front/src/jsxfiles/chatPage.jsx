4a86d049521615bcdf788ec438f482a2c6c37f17
jwlee <tlstprhl12@hansung.ac.kr>
2024-05-09 23:59:59 +0900

    fReco 매개변수

--- front/src/jsxfiles/chatPage.jsx
+++ front/src/jsxfiles/chatPage.jsx
@@ -314,8 +314,7 @@ const ChatPage = () => {
 
             setShowPdfViewer(true);
             setPdfUrl(pdfUrl);
-            const fReco = await getfReco();
-            console.log(fReco);
+            const fReco = await getfReco(selectedChatId);
             const formattedText = `사용자들이 많이 검색한 질문유형은 <${fReco.prediction ?? " "}>(이)에요!
 <${fReco.prediction ?? " "}> 유형에서 질문을 추천해 드릴게요!
   
